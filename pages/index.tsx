ab7af0e82efaa82844f9f4157f002de7f7ccb8cc
David Vargas <dvargas92495@gmail.com>
2021-09-01 22:30:49 -0400

    Default export Home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import ReactDOMServer from "react-dom/server";
-import fs from "fs";
 
 const Home: React.FunctionComponent = () => <div>Welcome!</div>;
 
-fs.writeFileSync("index.html", ReactDOMServer.renderToString(<Home/>));
+export default Home;
